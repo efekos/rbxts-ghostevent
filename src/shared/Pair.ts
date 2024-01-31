435ce2cd411b64d0ef5756e40ce4429ad6e903ab
efekos <hefeolgun10@gmail.com>
2024-01-31 16:26:41 +0300

    add Pair

---
+++ src/shared/Pair.ts
@@ -0,0 +1,13 @@
+export default class Pair<K,V> {
+    object1:K
+    object2:V
+
+    private constructor(obj1:K,obj2:V) {
+        this.object1 = obj1;
+        this.object2 = obj2;
+    }
+
+    public static of<K,V>(object1:K,object2:V):Pair<K,V>{
+        return new Pair(object1,object2);
+    }
+}
