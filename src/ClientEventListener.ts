869cff76a939fc7a655dfb78ed6998130895433a
efekos <hefeolgun10@gmail.com>
2024-01-31 17:44:38 +0300

    make RemoteEvent create on server

--- src/ClientEventListener.ts
+++ src/ClientEventListener.ts
@@ -8,9 +8,11 @@ export class ClientEventListener {
         if (ReplicatedStorage.FindFirstChild("Events") === undefined) new Instance("Folder", ReplicatedStorage).Name = "Events";
         const eventsFolder = ReplicatedStorage.WaitForChild("Events");
 
-        const event = new Instance("RemoteEvent", eventsFolder) as RemoteEvent;
-        event.Name = name;
+        const event = this.sendCreatePacket(name);
+        print("created event")
+        wait(0.01)
         event.FireServer(...args);
+        print("fire event")
     }
 
     static registerListener(name: string, callback: Callback): void {
@@ -21,14 +23,21 @@ export class ClientEventListener {
         return this.pairList.filter(r => r.object1 === name);
     }
 
+    private static sendCreatePacket(name:string):RemoteEvent{
+        const func = ReplicatedStorage.WaitForChild("ce") as RemoteFunction;
+        func.InvokeServer(name);
+        return ReplicatedStorage.WaitForChild("Events").WaitForChild(name) as RemoteEvent;
+    }
+
     static handle(): void {
         if (ReplicatedStorage.FindFirstChild("Events") === undefined) new Instance("Folder", ReplicatedStorage).Name = "Events";
         const eventsFolder = ReplicatedStorage.WaitForChild("Events");
 
         eventsFolder.ChildAdded.Connect(i => {
+            print("received event")
             if (!i.IsA("RemoteEvent")) return;
             const event = i as RemoteEvent;
-
+        
             const evnetList = this.pairList.filter(r => r.object1 === event.Name);
             let maxConnections = evnetList.size();
             let connections = 0;
